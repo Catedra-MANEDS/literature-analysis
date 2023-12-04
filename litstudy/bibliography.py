6a2f52b708e146c8df2bcd446a6fe8406c7d1fba
Pablo Manglano <pablo.manglano@edu.uah.es>
2023-12-04 11:36:22 +0100

    sync

--- litstudy/bibliography.py
+++ litstudy/bibliography.py
@@ -11,7 +11,7 @@ plt.rcParams['figure.figsize'] = (10, 6)
 sbs.set('paper')
 
 # Load the CSV files
-docs1 = litstudy.load_ieee_csv('bibliography/data/ieee.csv')
+docs1 = litstudy.load_ieee_csv('litstudy/data/ieee.csv')
 # docs2 = litstudy.load_ieee_csv('data/ieee_2.csv')
 # docs3 = litstudy.load_ieee_csv('data/ieee_3.csv')
 # docs4 = litstudy.load_ieee_csv('data/ieee_4.csv')
@@ -20,14 +20,14 @@ docs1 = litstudy.load_ieee_csv('bibliography/data/ieee.csv')
 docs_ieee = docs1
 print(len(docs_ieee), 'papers loaded from IEEE')
 
-docs_springer = litstudy.load_springer_csv('bibliography/data/springer.csv')
+docs_springer = litstudy.load_springer_csv('litstudy/data/springer.csv')
 print(len(docs_springer), 'papers loaded from Springer')
 
 # Merge the two document sets
 docs_csv = docs_ieee | docs_springer
 print(len(docs_csv), 'papers loaded from CSV')
 
-docs_exclude = litstudy.load_ris_file('bibliography/data/exclude.ris')
+docs_exclude = litstudy.load_ris_file('litstudy/data/exclude.ris')
 docs_remaining = docs_csv - docs_exclude
 
 print(len(docs_exclude), 'papers were excluded')
@@ -52,7 +52,9 @@ litstudy.compute_word_distribution(corpus).filter(like='_', axis=0).sort_index()
 litstudy.plot_word_distribution(corpus, limit=50, title="Top words", vertical=True, label_rotation=45);
 plt.show()
 
-litstudy.plot_cocitation_network(docs, max_edges=500)
+print(docs)
+
+litstudy.plot_cocitation_network(docs, max_edges=10)
 plt.show()
 
 litstudy.plot_country_histogram(docs, limit=15);
